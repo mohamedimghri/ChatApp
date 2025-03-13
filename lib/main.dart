a8b6e42fe5de71f9c77ddaa1362d364208204935
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-03-13 15:13:10 +0000

    add light mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chatapp/pages/login_page.dart';
+import 'package:chatapp/themes/light_mode.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,13 +8,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
+      theme: lightMode,
     );
   }
 }
