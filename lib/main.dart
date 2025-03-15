461cb07d476f3b8b388b633fb4e765448267d9dd
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-03-15 23:25:54 +0000

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chatapp/pages/login_page.dart';
+import 'package:chatapp/auth/login_or_register.dart';
 import 'package:chatapp/themes/light_mode.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: LoginOrRegister(),
       theme: lightMode,
     );
   }
