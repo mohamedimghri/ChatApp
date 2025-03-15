fa37321b16d724b0ccd543cc087d5d9502549925
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-03-15 23:24:52 +0000

    add login or register page

---
+++ lib/auth/login_or_register.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginOrRegister extends StatefulWidget {
+  const LoginOrRegister({super.key});
+
+  @override
+  State<LoginOrRegister> createState() => _LoginOrRegisterState();
+}
+
+class _LoginOrRegisterState extends State<LoginOrRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
