3a523b4a78082e0c6f8f911a497af3b9faf723c7
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-03-22 20:45:42 +0000

    remove logout from homePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,14 @@
-import 'package:chatapp/auth/auth_service.dart';
 import 'package:chatapp/components/my_drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  void logout() {
-    final _auth = AuthService();
-    _auth.signOut();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
-        actions: [
-          IconButton(onPressed: logout, icon: Icon(Icons.logout)),
-        ],
       ),
       drawer: MyDrawer(),
     );
