b540df7293dbf4aa6b3ec370e9ba9ca975f7e153
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-03-22 20:45:05 +0000

    add methode to home ,settings , logout

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -1,7 +1,14 @@
+import 'package:chatapp/auth/auth_service.dart';
+import 'package:chatapp/pages/settings_page.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
+
+    void logout() {
+    final _auth = AuthService();
+    _auth.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,47 +19,59 @@ class MyDrawer extends StatelessWidget {
         children: [
           Column(
             children: [
-
               //logo
-          DrawerHeader(
-            child: Center(
-              child: Icon(Icons.message,color: Theme.of(context).colorScheme.primary,size: 40,),
-            )
-          ),
+              DrawerHeader(
+                  child: Center(
+                child: Icon(
+                  Icons.message,
+                  color: Theme.of(context).colorScheme.primary,
+                  size: 40,
+                ),
+              )),
 
-        //home
-        Padding(
-          padding: EdgeInsets.only(left: 25.0),
-          child: ListTile(
-            title: Text("H O M E"),
-            leading: Icon(Icons.home),
-            onTap: (){},
-          ),
-          ),
+              //home
+              Padding(
+                padding: EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: Text("H O M E"),
+                  leading: Icon(Icons.home),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
 
-          //settings
-        Padding(
-          padding: EdgeInsets.only(left: 25.0),
-          child: ListTile(
-            title: Text("S E T T I N G S"),
-            leading: Icon(Icons.settings),
-            onTap: (){},
-          ),
-          ),
+              //settings
+              Padding(
+                padding: EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: Text("S E T T I N G S"),
+                  leading: Icon(Icons.settings),
+                  onTap: () {
+                    Navigator.pop(context);
+                    //to settings page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder:(context) => SettingsPage(), ) 
+                    );
+                  },
+                ),
+              ),
             ],
           ),
 
-      //logout
-        Padding(
-          padding: EdgeInsets.only(left: 25.0,bottom: 25),
-          child: ListTile(
-            title: Text("L O G O U T"),
-            leading: Icon(Icons.logout),
-            onTap: (){},
-          ),
+          //logout
+          Padding(
+            padding: EdgeInsets.only(left: 25.0, bottom: 25),
+            child: ListTile(
+              title: Text("L O G O U T"),
+              leading: Icon(Icons.logout),
+              onTap: logout ,
+            ),
           ),
         ],
       ),
-    ) ;
+    );
   }
 }
