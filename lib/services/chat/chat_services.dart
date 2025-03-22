deea712eeee43adbf1896fe8852f8c901a091d37
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-03-22 22:05:09 +0000

    add chat service with methode to getAllusers

---
+++ lib/services/chat/chat_services.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatServices {
+  //get instance of firebase
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  //get user stream
+  Stream<List<Map<String, dynamic>>> getUsersStream() {
+    return _firestore.collection("Users").snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        final user = doc.data();
+
+        return user;
+      }).toList();
+    });
+  }
+  //send message
+
+  //get message
+}
