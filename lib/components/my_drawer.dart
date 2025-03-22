d8b20ba8cf3dbebeb51a2f54be529cd1498c002e
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-03-22 00:10:09 +0000

    add home ,settings and logout buttons to the Drawer

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -8,12 +8,49 @@ class MyDrawer extends StatelessWidget {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.background,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
+          Column(
+            children: [
+
+              //logo
           DrawerHeader(
             child: Center(
-              child: Icon(Icons.message,color: Theme.of(context).colorScheme.primary,size: 64,),
+              child: Icon(Icons.message,color: Theme.of(context).colorScheme.primary,size: 40,),
             )
-          )
+          ),
+
+        //home
+        Padding(
+          padding: EdgeInsets.only(left: 25.0),
+          child: ListTile(
+            title: Text("H O M E"),
+            leading: Icon(Icons.home),
+            onTap: (){},
+          ),
+          ),
+
+          //settings
+        Padding(
+          padding: EdgeInsets.only(left: 25.0),
+          child: ListTile(
+            title: Text("S E T T I N G S"),
+            leading: Icon(Icons.settings),
+            onTap: (){},
+          ),
+          ),
+            ],
+          ),
+
+      //logout
+        Padding(
+          padding: EdgeInsets.only(left: 25.0,bottom: 25),
+          child: ListTile(
+            title: Text("L O G O U T"),
+            leading: Icon(Icons.logout),
+            onTap: (){},
+          ),
+          ),
         ],
       ),
     ) ;
