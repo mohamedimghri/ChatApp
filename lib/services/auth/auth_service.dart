d027a3194f0ad760e59c74fa3c132c390db2b812
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-03-25 13:52:19 +0000

    add methode to get The cuurent user

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -5,13 +5,18 @@ class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
+  //get current user
+  User? getCurrentUser() {
+    return _auth.currentUser;
+  }
+
   //sign in
   Future<UserCredential> signInWithEmailPassword(String email, password) async {
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
-          //svae user info if it doesn't already existe
-        _firestore.collection("Users").doc(userCredential.user!.uid).set({
+      //svae user info if it doesn't already existe
+      _firestore.collection("Users").doc(userCredential.user!.uid).set({
         'uid': userCredential.user!.uid,
         'email': email,
       });
