b3e085247f62d7f1073cc881a7917c424c9b7f87
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-03-25 15:08:37 +0000

    add input and button send to chatpage

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,8 +1,27 @@
+import 'package:chatapp/components/my_textfield.dart';
+import 'package:chatapp/services/auth/auth_service.dart';
+import 'package:chatapp/services/chat/chat_services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
   final String receiverEmail;
-  const ChatPage({super.key,required this.receiverEmail});
+  final String receiverID;
+  ChatPage({super.key, required this.receiverEmail, required this.receiverID});
+  //text controller
+  final TextEditingController _messgaeController = TextEditingController();
+
+  // chat & auth Services
+  final ChatServices _chatServices = ChatServices();
+  final AuthService _authService = AuthService();
+
+  //send message
+  void sendMessage() async {
+    if (_messgaeController.text.isNotEmpty) {
+      await _chatServices.sendMessage(receiverID, _messgaeController.text);
+      _messgaeController.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +29,52 @@ class ChatPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(receiverEmail),
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: _buildMessageList(),
+          ),
+          _buildUserInput(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMessageList() {
+    String senderID = _authService.getCurrentUser()!.uid;
+    return StreamBuilder(
+        stream: _chatServices.getMessages(receiverID, senderID),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text("Error");
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text("Loading..");
+          }
+
+          return ListView(
+            children: snapshot.data!.docs
+                .map((doc) => _buildMessageItem(doc))
+                .toList(),
+          );
+        });
+  }
+
+  Widget _buildMessageItem(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return Text(data["message"]);
+  }
+
+  Widget _buildUserInput() {
+    return Row(
+      children: [
+        Expanded(
+            child: MyTextfield(
+                hintText: "Type a message",
+                obscurText: false,
+                controllerText: _messgaeController)),
+        IconButton(onPressed: sendMessage, icon: Icon(Icons.arrow_upward))
+      ],
     );
   }
 }
