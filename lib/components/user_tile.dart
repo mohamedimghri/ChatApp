da16f527820c8e5e7ced6713ae419e9704742cb1
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-03-25 13:53:10 +0000

    styliser view of users

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -14,9 +14,12 @@ class UserTile extends StatelessWidget {
           color: Theme.of(context).colorScheme.secondary,
           borderRadius: BorderRadius.circular(12),
         ),
+        margin: const EdgeInsets.symmetric(vertical: 5,horizontal: 25),
+        padding: EdgeInsets.all(20),
         child: Row(
           children: [
             Icon(Icons.person),
+            SizedBox(width: 20,),
             Text(text),
           ],
         ),
